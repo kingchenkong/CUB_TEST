45e36495fe37b1205f5b4a9c6db3424c28159fb8
Kingchenkong <wikeyno@gmail.com>
2022-03-27 22:35:37 +0800

    kinfchenkonf
    Add: DetailActivity

---
+++ andorid/app/src/main/java/com/example/cub_test/view/DetailActivity.kt
@@ -0,0 +1,39 @@
+package com.example.cub_test.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import com.example.cub_test.R
+import com.example.cub_test.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    val TAG: String = DetailActivity::class.java.simpleName
+
+
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        // init method
+        processIntent()
+        setListener()
+
+    }
+
+    // init method
+    private fun processIntent() {
+
+    }
+
+    // listener
+    private fun setListener() {
+
+    }
+
+
+}
