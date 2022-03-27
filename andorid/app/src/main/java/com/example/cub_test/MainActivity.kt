b3141a369426a4732c218ee0197e9c1fa25b1a30
Kingchenkong <wikeyno@gmail.com>
2022-03-27 21:56:04 +0800

    kingchenkong
    Update: user list layout; add custom color, modify theme to NoActionBar.

--- andorid/app/src/main/java/com/example/cub_test/MainActivity.kt
+++ andorid/app/src/main/java/com/example/cub_test/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.cub_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cub_test.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val TAG: String = MainActivity::class.java.simpleName
+
+    // binding
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+
     }
 }
